b1184e376cb1e0eefc52e62dc1f44f36cc9a8747
Surendar Murthy <surendar.murthy@tigeranalytics.local>
2025-06-17 00:33:39 +0530

    db changes

--- server/TaskTracker/TaskTracker/settings.py
+++ server/TaskTracker/TaskTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*)%mojkinhhduy#yo56^+xkynd3d0j6d6t#-sbqkg#1()6i0$2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
