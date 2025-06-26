a9ee87f39a6472eeb81d82324a466506eb4aaede
Surendar Murthy <surendar.murthy@tigeranalytics.local>
2025-06-26 12:55:31 +0530

    new change

--- server/TaskTracker/TaskTracker/settings.py
+++ server/TaskTracker/TaskTracker/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware"
 ]
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://13.200.215.99:5173",
 ]
