871163ab518925248ebb800ac6e9865b1983d030
Surendar Murthy <surendar.murthy@tigeranalytics.local>
2025-06-26 12:06:35 +0530

    db change

--- server/TaskTracker/TaskTracker/settings.py
+++ server/TaskTracker/TaskTracker/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "*"
 ]
 
 ROOT_URLCONF = 'TaskTracker.urls'
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'TaskTracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tasktracker',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Surendar$20051996',
-#         'HOST': 'database-1.cx20wqm0s819.ap-south-1.rds.amazonaws.com',  # or your DB host
-#         'PORT': '5432',        # default PostgreSQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tasktracker',
+        'USER': 'postgres',
+        'PASSWORD': 'Surendar$20051996',
+        'HOST': 'database-1.cx20wqm0s819.ap-south-1.rds.amazonaws.com',  # or your DB host
+        'PORT': '5432',        # default PostgreSQL port
+    }
+}
 
 
 # Password validation
